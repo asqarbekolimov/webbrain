4684eaeae8627d0674a62761ca57c4a04066b0d8
ramizjo <ramizsheraliyev8@gmail.com>
2025-04-11 00:05:34 +0500

    update login and dashboard page

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import LoginPage from "./auth/login/page";
+import LoginPage from "./[lng]/auth/login/page";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
